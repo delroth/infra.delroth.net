3e9f49291d2ac9f3ba533f1adbe775bf633ef1f9
Pierre Bourdon <delroth@gmail.com>
2018-10-24 06:01:02 +0200

    monitoring: add a basic probing setup

--- monitoring.nix
+++ monitoring.nix
@@ -7,6 +7,11 @@
     listenAddress = "127.0.0.1:9090";
     webExternalUrl = "https://prom.delroth.net/";
 
+    exporters.blackbox = {
+      enable = true;
+      port = 9115;
+      configFile = ./blackbox.yml;
+    };
     exporters.node = {
       enable = true;
       enabledCollectors = [ "interrupts" "systemd" "tcpstat" ];
@@ -19,7 +24,27 @@
       port = 9130;
     };
 
-    scrapeConfigs = [
+    scrapeConfigs = let
+
+      blackboxTargets = {job_name, scrape_interval, modules, targets}: {
+        job_name = job_name;
+        scrape_interval = scrape_interval;
+        metrics_path = "/probe";
+        params = {
+          module = modules;
+        };
+        static_configs = [
+          { targets = targets; }
+        ];
+        relabel_configs = [
+          { source_labels = [ "__address__" ]; target_label = "__param_target"; }
+          { source_labels = [ "__param_target" ]; target_label = "instance"; }
+          { source_labels = []; target_label = "__address__";
+            replacement = "127.0.0.1:${toString exporters.blackbox.port}"; }
+        ];
+      };
+
+    in [
       {
         job_name = "node";
         scrape_interval = "10s";
@@ -34,6 +59,21 @@
           { targets = ["127.0.0.1:${toString exporters.tor.port}"]; }
         ];
       }
+
+      (blackboxTargets {
+        job_name = "http_probe";
+        scrape_interval = "1m";
+        modules = ["http_2xx"];
+        targets = [
+          "https://delroth.net"
+          "https://home.delroth.net"
+
+          "https://dolphin-emu.org"
+          "https://forums.dolphin-emu.org"
+          "https://wiki.dolphin-emu.org"
+          "https://dl.dolphin-emu.org"
+        ];
+      })
     ];
 
     alertmanager = {
