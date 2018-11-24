ce8c2f2b8b5542455b3375c163f8a8d853563d46
Pierre Bourdon <delroth@gmail.com>
2018-11-24 15:18:07 +0100

    eden: limit bandwidth, we've only got 1TB/month :<

--- machines/eden/default.nix
+++ machines/eden/default.nix
@@ -11,4 +11,10 @@ in {
     my.roles.syncthingRelay
     my.roles.torRelay
   ];
+
+  # Only 1TB/month to use :(
+  services.syncthing.relay.globalRateBps = 200 * 1024;  # 200KB/s
+  services.syncthing.relay.perSessionRateBps = 100 * 1024;  # 200KB/s
+  services.tor.relay.accountingStart = "day 0:00";
+  services.tor.relay.accountingMax = "10 GBytes";
 }
--- roles/syncthing-relay.nix
+++ roles/syncthing-relay.nix
@@ -11,8 +11,8 @@
 
     providedBy = "delroth (${machineName})";
 
-    globalRateBps = 20 * 1024 * 1024;  # 20MB/s
-    perSessionRateBps = 5 * 1024 * 1024;  # 5MB/s
+    globalRateBps = lib.mkDefault (20 * 1024 * 1024);  # 20MB/s
+    perSessionRateBps = lib.mkDefault (5 * 1024 * 1024);  # 5MB/s
   };
 
   networking.firewall.allowedTCPPorts = [
