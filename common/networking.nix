a823a61a957e28c2bc00d4d387fe44ecaaaa5e6f
Pierre Bourdon <delroth@gmail.com>
2018-12-04 23:46:02 +0100

    common/networking: switch to using DNS-over-TLS through Cloudflare

--- common/networking.nix
+++ common/networking.nix
@@ -3,6 +3,19 @@
 {
   networking.hostName = "${machineName}.delroth.net";
   networking.firewall.allowPing = true;
-  networking.nameservers = ["8.8.8.8" "8.8.4.4"];
   networking.search = [ "delroth.net" ];
+
+  # DNS to DNS-over-TLS gateway.
+  services.stubby = {
+    enable = true;
+    upstreamServers = ''
+      - address_data: 1.1.1.1
+        tls_auth_name: "cloudflare-dns.com"
+      - address_data: 1.0.0.1
+        tls_auth_name: "cloudflare-dns.com"
+    '';
+  };
+
+  # Send to local Stubby resolver.
+  networking.nameservers = [ "127.0.0.1" ];
 }
