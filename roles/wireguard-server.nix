3cb555da84d6411c27120cd0704e65d98f0277c7
Pierre Bourdon <delroth@gmail.com>
2019-02-02 13:36:47 +0100

    wireguard: configure peer public keys

--- roles/wireguard-server.nix
+++ roles/wireguard-server.nix
@@ -1,4 +1,4 @@
-{ config, machineName, ... }:
+{ config, lib, machineName, ... }:
 
 let
   my = import ../.;
@@ -6,6 +6,13 @@ in {
   networking.wireguard.interfaces.wg0 = {
     listenPort = 51820;
     privateKey = my.secrets.wireguard.privateKeys."${machineName}";
-    ips = [ "10.13.37.1/24" ];
+    ips = [ "10.13.37.254/24" ];
+
+    peers = map
+      (key: {
+        allowedIPs = [ "10.13.37.0/24" ];
+        publicKey = key;
+      })
+      (lib.attrValues my.secrets.wireguard.publicKeys);
   };
 }
--- secrets.nix
+++ secrets.nix
@@ -23,6 +23,7 @@ in
       key = builtins.readFile ./secrets/sso-key;
     };
     wireguard = {
+      publicKeys = import ./secrets/wireguard-clients.nix;
       privateKeys = import ./secrets/wireguard-keys.nix;
     };
   }
