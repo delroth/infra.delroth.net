03efc7672c94c8f760cf685837eec33a14053df0
Pierre Bourdon <delroth@gmail.com>
2019-02-22 00:23:06 +0100

    machines/chaos/networking: add secondary IP addresses for chaos-vpn-in

--- machines/chaos/networking.nix
+++ machines/chaos/networking.nix
@@ -4,14 +4,14 @@
   config = lib.mkIf (!staging) {
     networking.dhcpcd.enable = false;
     networking.interfaces.ens3 = {
-      ipv4.addresses = [{
-        address = "195.201.9.37";
-        prefixLength = 26;
-      }];
-      ipv6.addresses = [{
-        address = "2a01:4f8:13b:f15::1";
-        prefixLength = 64;
-      }];
+      ipv4.addresses = [
+        { address = "195.201.9.37"; prefixLength = 26; }
+        { address = "195.201.9.57"; prefixLength = 26; }
+      ];
+      ipv6.addresses = [
+        { address = "2a01:4f8:13b:f15::1"; prefixLength = 64; }
+        { address = "2a01:4f8:13b:f15::2"; prefixLength = 64; }
+      ];
     };
     networking.defaultGateway = "195.201.9.58";
     networking.defaultGateway6 = {
