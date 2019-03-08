cc977acc2192a3339b1ca26afda511c4013011e2
Pierre Bourdon <delroth@gmail.com>
2019-03-08 02:08:33 +0100

    chaos/dns: update ns2.afraid.org IP (transfer ACL)

--- machines/chaos/dns.nix
+++ machines/chaos/dns.nix
@@ -12,7 +12,7 @@
         name = "delroth.net";
         file = "/srv/dns/delroth.net.zone";
         slaves = [
-          "174.37.196.55"  # ns2.afraid.org
+          "69.65.50.192"  # ns2.afraid.org
           "204.42.254.5"   # puck.nether.net
         ];
       }
