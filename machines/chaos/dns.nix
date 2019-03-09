b7545d6da3d56280de71eaee3b3052bdcf23293b
Pierre Bourdon <delroth@gmail.com>
2019-03-09 02:51:07 +0100

    chaos/dns: add revdns zone

--- machines/chaos/dns.nix
+++ machines/chaos/dns.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }:
 
-{
+let
+  secondaryDnsServers = [
+    "69.65.50.192"  # ns2.afraid.org
+    "204.42.254.5"  # puck.nether.net
+  ];
+in {
   services.bind = {
     enable = true;
     extraOptions = ''
@@ -11,10 +16,13 @@
         master = true;
         name = "delroth.net";
         file = "/srv/dns/delroth.net.zone";
-        slaves = [
-          "69.65.50.192"  # ns2.afraid.org
-          "204.42.254.5"   # puck.nether.net
-        ];
+        slaves = secondaryDnsServers;
+      }
+      {
+        master = true;
+        name = "6.2.4.6.8.6.1.0.2.0.a.2.ip6.arpa";
+        file = "/srv/dns/6.2.4.6.8.6.1.0.2.0.a.2.ip6.arpa.zone";
+        slaves = secondaryDnsServers;
       }
     ];
   };
