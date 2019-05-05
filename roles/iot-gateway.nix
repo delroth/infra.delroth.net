918f13d8725eba962698af6e2dcd71378972f336
Pierre Bourdon <delroth@gmail.com>
2019-05-05 02:18:44 +0200

    iot-gateway: add nginx configuration

--- roles/iot-gateway.nix
+++ roles/iot-gateway.nix
@@ -3,6 +3,9 @@
 let
   cfg = config.my.roles.iot-gateway;
   my = import ../.;
+
+  hostname = "hass.delroth.net";
+  port = 8123;
 in {
   options.my.roles.iot-gateway = {
     enable = lib.mkEnableOption "IoT gateway";
@@ -13,6 +16,13 @@ in {
       enable = true;
 
       config = {
+        http = {
+          server_host = "127.0.0.1";
+          server_port = port;
+          base_url = "https://${hostname}";
+          use_x_forwarded_for = true;
+          trusted_proxies = [ "127.0.0.1" ];
+        };
         frontend = {};
         history = {};
 
@@ -40,6 +50,24 @@ in {
           }
           { platform = "netatmo"; }
         ];
+      };
+    };
+
+    services.nginx.virtualHosts."${hostname}" = {
+      forceSSL = true;
+      enableACME = true;
+
+      locations."/" = {
+        proxyPass = "http://localhost:${toString port}";
+        extraConfig = ''
+          proxy_http_version 1.1;
+          proxy_redirect http:// https://;
+          proxy_set_header Host $host;
+          proxy_set_header Upgrade $http_upgrade;
+          proxy_set_header Connection "Upgrade";
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_buffering off;
+        '';
       };
     };
   };
