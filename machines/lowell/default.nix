5a9ed7609c7ef04be2ab8dc1e88f461be280559a
Pierre Bourdon <delroth@gmail.com>
2019-05-22 13:23:19 +0900

    lowell: switch to stable chromium, beta doesn't currently build

--- machines/lowell/default.nix
+++ machines/lowell/default.nix
@@ -23,7 +23,7 @@ in {
   nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = with pkgs; [
-    wget chromiumBeta xterm most mpv feh pavucontrol acpi gitFull dex gnupg
+    wget chromium xterm most mpv feh pavucontrol acpi gitFull dex gnupg
     cifs-utils tpm2-tools git-crypt python3 pwgen keepassxc vulnix electrum
     lm_sensors xorg.xbacklight picocom whois transmission scrot imgurbash2
     my.pkgs.vim nixops gnome3.eog evince libnotify hexedit my.pkgs.blitzloop
