75242ab43031b230b12711795dc56f7b7ffc8f53
Pierre Bourdon <delroth@gmail.com>
2019-09-14 15:37:11 +0200

    secrets: use Nix 2.3's hashFile builtin

--- secrets.nix
+++ secrets.nix
@@ -1,9 +1,6 @@
 let
   pkgs = import <nixpkgs> {};
-  canaryHashDerivation = pkgs.runCommand "secrets-canary-hash" {} ''
-    sha256sum "${./secrets/canary}" | cut -d ' ' -f 1 | tr -d "\n" > $out
-  '';
-  canaryHash = builtins.readFile canaryHashDerivation;
+  canaryHash = builtins.hashFile "sha256" ./secrets/canary;
   expectedHash = "27a6153adee6291f22bd145f29a5596cef0e87abbc87762576204bc4c8a1cf93";
 in
   if canaryHash != expectedHash then abort "Secrets are not readable. Have you run `git-crypt unlock`?"
