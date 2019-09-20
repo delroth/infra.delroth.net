79516a8023308349ae36a1e0cd244919234c55c5
Pierre Bourdon <delroth@gmail.com>
2019-09-20 21:05:28 +0200

    laptop: enable dhcpcd noarp

--- common/laptop.nix
+++ common/laptop.nix
@@ -15,6 +15,12 @@ in {
   boot.extraModulePackages = [ config.boot.kernelPackages.acpi_call ];
   boot.kernelModules = [ "acpi_call" ];
 
+  networking.dhcpcd.extraConfig = ''
+    # Skip ARP probing and trust the DHCP server to have given us a valid
+    # assignment. Improves time to network availability.
+    noarp
+  '';
+
   # Set a password for the main login user.
   users.users.delroth.hashedPassword = my.secrets.shadowHash;
 }
