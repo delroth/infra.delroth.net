ebd90621952d1197e0218c53df6e3759473fffbd
Pierre Bourdon <delroth@gmail.com>
2019-09-21 01:33:58 +0200

    machines: remove nixos-installer, the way this was integrated isn't great

--- machines/default.nix
+++ machines/default.nix
@@ -1,11 +1,7 @@
 {
-  # Actual machines.
   chaos = ./chaos;
   duke = ./duke;
   eden = ./eden;
   lowell = ./lowell;
   yew = ./yew;
-
-  # Targets that can be used to build ISOs / disk images.
-  nixos-installer = ./nixos-installer;
 }
