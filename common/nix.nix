341d55cfb7741567937915249fab50690aee46cc
Pierre Bourdon <delroth@gmail.com>
2019-09-24 01:35:41 +0200

    nix: provide a command-not-found database (hardcoded from a channel)

--- common/nix.nix
+++ common/nix.nix
@@ -25,4 +25,18 @@ in {
 
   # Add custom package set to overlays.
   nixpkgs.overlays = [ my.pkgs ];
+
+  # Add support for command-not-found. For simplicity, hardcode a Nix channel
+  # revision that has the programs.sqlite pregenerated instead of building it
+  # ourselves since that's expensive.
+  environment.variables.NIX_AUTO_RUN = "1";
+  programs.command-not-found.dbPath = let
+    channelTarball = pkgs.fetchurl {
+      url = "https://releases.nixos.org/nixos/unstable/nixos-20.03pre193781.d484f2b7fc0/nixexprs.tar.xz";
+      sha256 = "0aqm56p66ys3nhy19j5zbj8agfg7dyccnxgplm5kdykv22p8h4gc";
+    };
+  in
+    pkgs.runCommand "programs.sqlite" {} ''
+      tar xf ${channelTarball} --wildcards "nixos*/programs.sqlite" -O > $out
+    '';
 }
