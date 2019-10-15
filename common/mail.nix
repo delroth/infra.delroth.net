f0a65ab14b93520028c5cd2705819c515260f287
Pierre Bourdon <delroth@gmail.com>
2019-10-16 01:13:50 +0200

    mail: dynamically generate the list of nullmailer remotes

--- common/mail.nix
+++ common/mail.nix
@@ -1,27 +1,39 @@
-{ config, machineName, secrets, ... }:
+{ config, lib, machineName, nodes, secrets, ... }:
 
 {
-  # nullmailer needs patching to function under scudo.
-  nixpkgs.overlays = [(self: super: {
-    nullmailer = super.nullmailer.overrideAttrs (old: {
-      patches = [
-        (self.fetchurl {
-          url = "https://github.com/delroth/nullmailer/commit/834e2eb6b7eac2648fc371c432a46e98d5966bb4.patch";
-          sha256 = "0z8jwfc0qa4nf8am06xlvff4xphra5chan37g9s4mlra78s1gwm7";
-        })
-      ];
-    });
-  })];
+  # We don't need an MTA on machines running postfix for SMTP servers.
+  config = lib.mkIf (!config.my.roles.smtp-server.enable) {
+    # nullmailer needs patching to function under scudo.
+    nixpkgs.overlays = [(self: super: {
+      nullmailer = super.nullmailer.overrideAttrs (old: {
+        patches = [
+          (self.fetchurl {
+            url = "https://github.com/delroth/nullmailer/commit/834e2eb6b7eac2648fc371c432a46e98d5966bb4.patch";
+            sha256 = "0z8jwfc0qa4nf8am06xlvff4xphra5chan37g9s4mlra78s1gwm7";
+          })
+        ];
+      });
+    })];
 
-  services.nullmailer = {
-    enable = true;
-    config = {
-      adminaddr = "root@delroth.net";
-      allmailfrom = "${machineName}@${machineName}.delroth.net";
-      defaultdomain = "delroth.net";
-      defaulthost = machineName;
-      me = config.networking.hostName;
-      remotes = "chaos.delroth.net smtp --port=25 --starttls --user=${secrets.email.smtp-user} --pass=${secrets.email.smtp-password}";
+    services.nullmailer = {
+      enable = true;
+      config = {
+        adminaddr = "root@delroth.net";
+        allmailfrom = "${machineName}@${machineName}.delroth.net";
+        defaultdomain = "delroth.net";
+        defaulthost = machineName;
+        me = config.networking.hostName;
+
+        remotes = let
+          hasSmtp = node: node.config.my.roles.smtp-server.enable;
+          nodesWithSmtp = builtins.filter hasSmtp (builtins.attrValues nodes);
+          lines = map (node:
+              "${node.config.networking.hostName} smtp --port=25 --starttls " +
+              "--user=${secrets.email.smtp-user} " +
+              "--pass=${secrets.email.smtp-password}") nodesWithSmtp;
+        in
+          builtins.concatStringsSep "\n" lines;
+      };
     };
   };
 }
