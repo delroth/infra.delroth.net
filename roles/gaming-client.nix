08ca41c23c52bd328bed11da217058249a34c7b5
Pierre Bourdon <delroth@gmail.com>
2020-03-15 05:51:52 +0100

    gaming-client: allow Steam IHS through the firewall

--- roles/gaming-client.nix
+++ roles/gaming-client.nix
@@ -11,5 +11,9 @@ in {
     environment.systemPackages = with pkgs; [ chiaki runelite steam ];
     hardware.opengl.driSupport32Bit = true;
     hardware.pulseaudio.support32Bit = true;
+
+    # Firewall ports used by Steam in-home streaming.
+    networking.firewall.allowedTCPPorts = [ 27036 27037 ];
+    networking.firewall.allowedUDPPorts = [ 27031 27036 ];
   };
 }
