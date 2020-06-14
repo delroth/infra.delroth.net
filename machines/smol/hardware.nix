e8328f226cda9ba543a97fc2dbb789730c40b878
Pierre Bourdon <delroth@gmail.com>
2020-06-14 05:41:00 +0200

    smol: add a fan spinning systemd unit

--- machines/smol/hardware.nix
+++ machines/smol/hardware.nix
@@ -30,6 +30,19 @@ in {
     options = [ "noatime" "discard" ];
   };
 
+  # Set fans at full speed on startup. Ideally this should be done by a kernel
+  # module instead which provides fan PWM control, but that's for later.
+  systemd.services.spin-fans = {
+    description = "Spin up the fans, full throttle.";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "multi-user.target" ];
+    serviceConfig.Type = "oneshot";
+    script = ''
+      ${pkgs.coreutils}/bin/stty -F /dev/ttyS1 115200
+      echo -ne '\x35' > /dev/ttyS1
+    '';
+  };
+
   nix.maxJobs = lib.mkDefault 4;
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
 }
