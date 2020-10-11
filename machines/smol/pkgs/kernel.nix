4eddc1c81d0514059e28068b21c1aafbc7f69c48
Pierre Bourdon <delroth@gmail.com>
2020-10-11 15:06:30 +0200

    smol: pin kernel git revision, current HEAD is broken

--- machines/smol/pkgs/kernel.nix
+++ machines/smol/pkgs/kernel.nix
@@ -7,7 +7,7 @@ buildLinux (args // rec {
   src = fetchFromGitHub {
     owner = "delroth";
     repo = "linux-qnap-tsx32x";
-    rev = "qnap-tsx32x";
+    rev = "5e6bbb3151d7cee2b89da8b9735488761d9eafdf";
     sha256 = "1hx2fd86zdia6mzgxx8xilsiv90dam4sd7ibis3z7ils8sz1vvwz";
   };
 
