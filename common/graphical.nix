257a992ae9bbb8a07449f9635ff6a0e40b3cbff9
Pierre Bourdon <delroth@gmail.com>
2020-12-05 19:31:48 +0100

    graphical: remove now unneeded mpv patches (new upstream release)

--- common/graphical.nix
+++ common/graphical.nix
@@ -39,21 +39,5 @@
 
     environment.systemPackages = with pkgs; [ alacritty ];
     environment.sessionVariables.TERMINAL = "alacritty";
-
-    # Patch mpv to avoid TrueHD buffering issues on CIFS.
-    nixpkgs.overlays = [(self: super: {
-      mpv-unwrapped = super.mpv-unwrapped.overrideAttrs (old: {
-        patches = old.patches ++ [
-          (self.fetchpatch {
-            url = "https://github.com/mpv-player/mpv/commit/c59ca06a0fff432ac4cae012bb0299a8db9a00d3.patch";
-            sha256 = "0iq9bafylmj129pb8v6p8gxmjaw8nisiixfxc0ri2bhv905wqc45";
-          })
-          (self.fetchpatch {
-            url = "https://github.com/mpv-player/mpv/commit/20eead18130fd460d8e9eff50ce14afd3646faab.patch";
-            sha256 = "16w77nvnq1awkkdnzd3nk2n2n4s22xmcz04d62jcpjq70r244xfd";
-          })
-        ];
-      });
-    })];
   };
 }
