ce9213abdb0128048b26c2c760424be5a0a9db67
Pierre Bourdon <delroth@gmail.com>
2020-12-06 22:46:14 +0100

    parsec: add a hack to fetch the latest release

--- pkgs/parsec.nix
+++ pkgs/parsec.nix
@@ -10,6 +10,24 @@ stdenv.mkDerivation {
     sha256 = "1hfdzjd8qiksv336m4s4ban004vhv00cv2j461gc6zrp37s0fwhc";
   };
 
+  # The upstream deb package is out of date and doesn't work out of the box
+  # anymore due to api.parsecgaming.com being down. Auto-updating doesn't work
+  # because it doesn't patchelf the dynamic dependencies. Instead, "manually"
+  # fetch the latest binaries.
+  latest_appdata = fetchurl {
+    url = "https://builds.parsecgaming.com/channel/release/appdata/linux/latest";
+    sha256 = "1hs934lqgh9brzx8b0hbir5jkvd9gl361w6098ycd64c1v18d77q";
+  };
+  latest_parsecd_so = fetchurl {
+    url = "https://builds.parsecgaming.com/channel/release/binary/linux/gz/parsecd-150-50.so";
+    sha256 = "1l5l9flgmq53lv7am36bg40wkchcjhvd7wzs71an111xxrcrvdfx";
+  };
+
+  postPatch = ''
+    cp $latest_appdata usr/share/parsec/skel/appdata.json
+    cp $latest_parsecd_so usr/share/parsec/skel/parsecd-150-50.so
+  '';
+
   runtimeDependencies = [
     alsaLib (lib.getLib dbus) libGL libpulseaudio libva.out
     (lib.getLib stdenv.cc.cc) (lib.getLib udev)
