376db62fd77ec681c229d6f18690a1810f5fa70f
Pierre Bourdon <delroth@gmail.com>
2021-01-13 03:10:57 +0100

    common/glome: services.mingetty -> services.getty (nixpkgs change)

--- common/glome.nix
+++ common/glome.nix
@@ -2,6 +2,6 @@
 
 let
 in {
-  services.mingetty.loginProgram = "${pkgs.glome}/bin/glome-login";
-  services.mingetty.loginOptions = "-l ${pkgs.shadow}/bin/login -k ${secrets.glome.service-public} -- \\u";
+  services.getty.loginProgram = "${pkgs.glome}/bin/glome-login";
+  services.getty.loginOptions = "-l ${pkgs.shadow}/bin/login -k ${secrets.glome.service-public} -- \\u";
 }
