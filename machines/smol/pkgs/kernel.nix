82ceb6bcee123706d37d12df328612a6c34d67cb
Pierre Bourdon <delroth@gmail.com>
2021-04-20 22:57:22 +0200

    smol: kernel 5.7.2 -> 5.11.15

--- machines/smol/pkgs/kernel.nix
+++ machines/smol/pkgs/kernel.nix
@@ -1,14 +1,14 @@
 { buildLinux, fetchFromGitHub, ... } @ args:
 
 buildLinux (args // rec {
-  version = "5.7.2";
+  version = "5.11.15";
   modDirVersion = "${version}-qnap";
 
   src = fetchFromGitHub {
     owner = "delroth";
     repo = "linux-qnap-tsx32x";
-    rev = "5e6bbb3151d7cee2b89da8b9735488761d9eafdf";
-    sha256 = "1hx2fd86zdia6mzgxx8xilsiv90dam4sd7ibis3z7ils8sz1vvwz";
+    rev = "qnap-tsx32x";
+    sha256 = "08vznjal7bvywizaz3l7pqj7na7qjdmkspdbls2rm93rczfzy5wj";
   };
 
   defconfig = "qnap-tsx32x_defconfig";
