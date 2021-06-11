6e6283969fb9ee57fa76292c1eedb71b10dd0e51
Pierre Bourdon <delroth@gmail.com>
2021-06-11 02:42:48 +0200

    parsec: move to secrets/, this is experimental and I'm tired of being asked for support

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,7 +2,6 @@ self: super: {
   blitzloop = super.callPackage ./blitzloop.nix {};
   glome = super.callPackage ./glome.nix {};
   glome-login-authorize = super.callPackage ./glome-login-authorize.nix {};
-  parsec = super.callPackage ./parsec.nix {};
   publibike-locator = super.callPackage ./publibike-locator.nix {};
   repology-notifier = super.callPackage ./repology-notifier.nix {};
   vim_delroth = super.callPackage ./vim.nix {};
