58fc0b1343d2b8a2f3df8fd2069a47bed09e4d76
Pierre Bourdon <delroth@gmail.com>
2021-07-19 05:41:12 +0200

    aether: create dpaa network interfaces in initrd

--- machines/aether/hardware.nix
+++ machines/aether/hardware.nix
@@ -30,6 +30,22 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Setup SFP+ network interfaces early so systemd can pick everything up.
+  boot.initrd.extraUtilsCommands = ''
+    copy_bin_and_libs ${pkgs.restool}/bin/restool
+    copy_bin_and_libs ${pkgs.restool}/bin/ls-main
+    copy_bin_and_libs ${pkgs.restool}/bin/ls-addni
+
+    # Patch paths
+    sed -i "1i #!$out/bin/sh" $out/bin/ls-main
+  '';
+  boot.initrd.postDeviceCommands = ''
+    ls-addni dpmac.7
+    ls-addni dpmac.8
+    ls-addni dpmac.9
+    ls-addni dpmac.10
+  '';
+
   fileSystems."/" = {
     device = "/dev/disk/by-label/root";
     fsType = "ext4";
