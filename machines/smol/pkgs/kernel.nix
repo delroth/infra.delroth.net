cedaa7e0057215289d67b3ed8a71c5cc3cf605f9
Pierre Bourdon <delroth@gmail.com>
2021-07-30 22:46:14 +0200

    smol: kernel bump

--- machines/smol/pkgs/kernel.nix
+++ machines/smol/pkgs/kernel.nix
@@ -8,7 +8,7 @@ buildLinux (args // rec {
     owner = "delroth";
     repo = "linux-qnap-tsx32x";
     rev = "qnap-tsx32x";
-    sha256 = "1y3f6my4h0yl6022jhqlkpdm3cynd4qns3yr8glpklbfa2a8a933";
+    sha256 = "1kpj73wjs47f8fwll24snw14c4m9d6kmkkyrbn14d4mbdklnyrxb";
   };
 
   defconfig = "qnap-tsx32x_defconfig";
