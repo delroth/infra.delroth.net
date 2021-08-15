2c8b7f492b64565af7cc0dbba4165ed1e8d286f8
Pierre Bourdon <delroth@gmail.com>
2021-08-15 04:18:45 +0200

    aether: rename network interfaces

--- machines/aether/networking.nix
+++ machines/aether/networking.nix
@@ -1,23 +1,6 @@
 { pkgs, ... }:
 
 let
-  iface = {
-    # Default route. Get public v4/32 + v6/48 from there and route/NAT.
-    # Current: top port on MLX CX4 PCIe NIC.
-    upstream = "enP4p1s0f0np0";
-
-    # Serves a simple DHCP for debug access if things are broken.
-    # Current: Honeycomb 1G copper port.
-    mgmt = "eth0";
-
-    # List of downstreams to bridge.
-    # Current: 4x Honeycomb 10G SFP+ + bottom port on MLX CX4 PCIe NIC.
-    downstreams = [ "eth1" "eth2" "eth3" "eth4" "enP4p1s0f1np1" ];
-
-    # Name of the downstream bridge.
-    bridge = "downstream";
-  };
-
   # v4 internal IP ranges. v6 uses prefix delegation from upstream
   range = {
     mgmt = {
@@ -41,13 +24,51 @@ let
     };
   };
 in {
-  networking.interfaces."${iface.upstream}" = {
+  # Rename interfaces to logical semantic names.
+  systemd.network.links = {
+    "10-upstream" = {
+      matchConfig.MACAddress = "50:6b:4b:38:93:ea";
+      linkConfig.Name = "upstream";
+    };
+    "10-mgmt" = {
+      matchConfig.MACAddress = "ee:31:06:c8:00:11";
+      linkConfig.Name = "mgmt";
+    };
+    "10-down-25g" = {
+      matchConfig.MACAddress = "50:6b:4b:38:93:eb";
+      linkConfig.Name = "down-25g";
+    };
+    "10-down-10g-tl" = {
+      matchConfig.MACAddress = "ee:31:06:c8:00:09";
+      linkConfig.Name = "down-10g-tl";
+    };
+    "10-down-10g-tr" = {
+      matchConfig.MACAddress = "ee:31:06:c8:00:07";
+      linkConfig.Name = "down-10g-tr";
+    };
+    "10-down-10g-bl" = {
+      matchConfig.MACAddress = "ee:31:06:c8:00:0a";
+      linkConfig.Name = "down-10g-bl";
+    };
+    "10-down-10g-br" = {
+      matchConfig.MACAddress = "ee:31:06:c8:00:08";
+      linkConfig.Name = "down-10g-br";
+    };
+  };
+
+  networking.interfaces.upstream = {
     useDHCP = true;
   };
 
-  networking.bridges."${iface.bridge}".interfaces = iface.downstreams;
+  networking.bridges.downstream.interfaces = [
+    "down-25g"
+    "down-10g-tl"
+    "down-10g-tr"
+    "down-10g-bl"
+    "down-10g-br"
+  ];
 
-  networking.interfaces."${iface.bridge}" = {
+  networking.interfaces.downstream = {
     ipv4.addresses = [
       {
         address = range.downstream.ip;
@@ -56,7 +77,7 @@ in {
     ];
   };
 
-  networking.interfaces."${iface.mgmt}" = {
+  networking.interfaces.mgmt = {
     ipv4.addresses = [
       {
         address = range.mgmt.ip;
@@ -73,7 +94,7 @@ in {
 
   services.dhcpd4 = {
     enable = true;
-    interfaces = [ iface.bridge iface.mgmt ];
+    interfaces = [ "downstream" "mgmt" ];
     configFile = pkgs.writeText "dhcpd4.conf" ''
       authoritative;
       default-lease-time 3600;
