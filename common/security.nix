d754c05be2d6132fd793720ede8e46bd501bc47d
Pierre Bourdon <delroth@gmail.com>
2021-12-17 11:58:10 +0100

    security: remove haveged, obsolete since Linux 5.10

--- common/security.nix
+++ common/security.nix
@@ -20,8 +20,6 @@ in {
     security.sudo.wheelNeedsPassword =  # Require password if set.
       config.users.users.delroth.hashedPassword != null;
 
-    services.haveged.enable = true;
-
     # TODO: Once more build capacity has been converted to NixOS, add hostname
     # to the seed as well for more diversity.
     boot.kernel.randstructSeed = "${secrets.randstructSeed}";
