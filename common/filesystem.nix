522fda420367c423bceeb3a1949aa3452e4154af
Pierre Bourdon <delroth@gmail.com>
2023-05-13 18:27:30 +0200

    treewide: fix deprecation warnings

--- common/filesystem.nix
+++ common/filesystem.nix
@@ -1,5 +1,5 @@
 { ... }:
 
 {
-  boot.cleanTmpDir = true;
+  boot.tmp.cleanOnBoot = true;
 }
--- common/remote-access.nix
+++ common/remote-access.nix
@@ -3,7 +3,7 @@
 {
   services.openssh = {
     enable = true;
-    passwordAuthentication = false;
+    settings.PasswordAuthentication = false;
   };
   programs.mosh.enable = true;
 }
