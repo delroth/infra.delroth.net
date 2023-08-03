846fdcf39ff9e65818ebb9bba16fc293f0cad975
Pierre Bourdon <delroth@gmail.com>
2023-08-03 21:47:57 +0200

    nginx: make non-optional, it causes circular dependencies and monitoring relies on it anyway

--- common/nginx.nix
+++ common/nginx.nix
@@ -1,24 +1,20 @@
 { config, lib, ... }:
 
 {
-  # Whenever something defines an nginx vhost, ensure that nginx defaults are
-  # properly set.
-  config = lib.mkIf ((builtins.attrNames config.services.nginx.virtualHosts) != []) {
-    services.nginx = {
-      enable = true;
-      statusPage = true;  # For monitoring scraping.
+  services.nginx = {
+    enable = true;
+    statusPage = true;  # For monitoring scraping.
 
-      recommendedGzipSettings = true;
-      recommendedOptimisation = true;
-      recommendedTlsSettings = true;
-    };
-
-    services.prometheus.exporters.nginx = {
-      enable = true;
-      listenAddress = "127.0.0.1";
-      port = 9113;
-    };
-
-    networking.firewall.allowedTCPPorts = [ 80 443 ];
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedTlsSettings = true;
   };
+
+  services.prometheus.exporters.nginx = {
+    enable = true;
+    listenAddress = "127.0.0.1";
+    port = 9113;
+  };
+
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
 }
