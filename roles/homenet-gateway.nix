500e148c1b3314dc91dda4adf17e46c5ae292f33
Pierre Bourdon <delroth@gmail.com>
2024-11-30 21:48:59 +0100

    homenet-gateway: start work on IGMP proxying

--- roles/homenet-gateway.nix
+++ roles/homenet-gateway.nix
@@ -260,6 +260,18 @@ in
       reflector = true;
     };
 
+    # Enable IGMP proxying. Note that default NixOS kernels do not enable
+    # CONFIG_IP_MROUTE by default.
+    #
+    # TODO: actual IGMP proxying.
+    boot.kernelPatches = [{
+      name = "enable_ip_mroute";
+      patch = null;
+      extraStructuredConfig = {
+        IP_MROUTE = lib.kernel.yes;
+      };
+    }];
+
     # We define our own nftables-based firewall ruleset.
     networking.nat.enable = false;
     networking.firewall.enable = false;
