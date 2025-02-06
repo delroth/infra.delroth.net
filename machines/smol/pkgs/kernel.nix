8e60f2d2da8b425ebece91d7cf951f5fee4f80a0
Pierre Bourdon <delroth@gmail.com>
2025-02-06 06:06:46 +0100

    smol: bump kernel to 6.12.12

--- machines/smol/pkgs/kernel.nix
+++ machines/smol/pkgs/kernel.nix
@@ -3,14 +3,14 @@
 buildLinux (
   args
   // rec {
-    version = "6.0.6";
+    version = "6.12.12";
     modDirVersion = "${version}-qnap";
 
     src = fetchFromGitHub {
       owner = "delroth";
       repo = "linux-qnap-tsx32x";
       rev = "qnap-tsx32x";
-      hash = "sha256-XjmjFKDWCqMw7lK1nsQ50f4cxdqklMRRdhDo7tDerM8=";
+      hash = "sha256-zGr41HenrgQZ3beLjp4kk2mU7nr9T/NT0nPAZSn4MDQ=";
     };
 
     defconfig = "qnap-tsx32x_defconfig";
