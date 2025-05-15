c002c2b5b710e55fb7a65bf93702b757212530de
Pierre Bourdon <delroth@gmail.com>
2025-05-15 10:51:54 +0200

    smol: roll back kernel to 6.0.2 due to IRQ/MSI-X issues

--- machines/smol/pkgs/kernel.nix
+++ machines/smol/pkgs/kernel.nix
@@ -3,23 +3,17 @@
 buildLinux (
   args
   // rec {
-    version = "6.12.12";
+    version = "6.0.6";
     modDirVersion = "${version}-qnap";
 
     src = fetchFromGitHub {
       owner = "delroth";
       repo = "linux-qnap-tsx32x";
-      rev = "qnap-tsx32x";
-      hash = "sha256-zGr41HenrgQZ3beLjp4kk2mU7nr9T/NT0nPAZSn4MDQ=";
+      rev = "old-6.0.6";
+      hash = "sha256-XjmjFKDWCqMw7lK1nsQ50f4cxdqklMRRdhDo7tDerM8=";
     };
 
     defconfig = "qnap-tsx32x_defconfig";
-    kernelPatches = [
-      {
-        name = "drm-panic-rust-compile-fix";
-        patch = ./drm-panic-rust-compile-fix.patch;
-      }
-    ];
     ignoreConfigErrors = true;
   }
 )
